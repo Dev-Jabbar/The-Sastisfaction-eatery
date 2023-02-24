742f8be81be9289c528d6f4ad67e446898deaaa6
MOMOH ABDUL JABBAR <momohabduljabbar@gmail.com>
2023-02-24 11:52:34 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+
+      fontFamily: {
+        body: ["Nunito"],
+      },
+    },
+  },
+  plugins: [],
+};
